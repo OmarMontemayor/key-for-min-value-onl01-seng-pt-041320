51a27bd25be92f7a22a86072eeb8ad9953e46fa4
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-25 00:09:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  array = []
-  value1 = 0
-  value2 = 1
   name_hash.collect do |key, value|
     if name_hash == {}
       return nil
