2cd01fefe6a7047de3d7b7ca9d95d0bbd84a53e2
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-25 00:17:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+value1 = 1
 def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     if name_hash == {}
       return nil
-    end
+    elsif value < value1
+      value1 = value
+      
   end  
   binding.pry
 end
