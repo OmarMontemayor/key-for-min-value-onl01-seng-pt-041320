7ae43836e61b0d98a4e13bcfafa09d5d635a9e8f
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-25 00:23:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,6 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     if name_hash == {}
       return nil
-    else
-      if value < value1
-        value1 = value
-        return key
-        binding.pry
-      end
     end
   end  
 end
